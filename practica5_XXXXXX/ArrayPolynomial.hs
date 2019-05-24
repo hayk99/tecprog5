f7047e12df568288b7cc04552ebc431930f0c121
hayk99 <haykk1999@gmail.com>
2019-05-24 12:32:59 +0200

    lo he dejado bonito y fin 5

--- practica5_XXXXXX/ArrayPolynomial.hs
+++ practica5_XXXXXX/ArrayPolynomial.hs
@@ -1,20 +1,30 @@
+{-
+	AUTORES: HAYK KOCHARYAN (757715) & JAVIER SALAMERO SANZ (756868)
+	PRACTICA 5 TECNOLOGÍA DE LA PROGRAMACIÓN
+	FECHA DE MODIFICACIÓN: 24 / 05 / 2019
+-}
 module ArrayPolynomial where
 
 type Polinomio = [Float]
 
 --definir funciones
  
+--Fucion que devuelve polinomio de grado 1
 x :: Polinomio
 x = [1,0]
 
+--Funcion que devuelve polinomio de grado 0
 coef :: Float -> Polinomio
 coef c = [c]
 
+--Funcion auxiliar para sumarLength
 zipWith' ::  Polinomio -> Polinomio -> Int -> Int -> Polinomio
 zipWith' p1 p2 longp1 longp2 = if (longp1 > longp2)
 								then [head p1] ++ zipWith' (tail p1) p2 (longp1-1) longp2
 							   else zipWith (+) p1 p2
 
+--Funcion auxiliar, aplicando inmersion, que dado dos polinomios y sus longitudes correspondientes devuleve 
+--la suma de estos dos
 sumarLength :: Polinomio -> Int -> Polinomio -> Int -> Polinomio
 sumarLength p1 longp1 p2 longp2 = if (longp1 >= longp2)
 								  		--then zipWith (+) p1 p2
@@ -22,25 +32,29 @@ sumarLength p1 longp1 p2 longp2 = if (longp1 >= longp2)
 								  		--else sumarLength p2 longp2 (headp1:p1) longp1
 								  		else zipWith' p2 p1 longp2 longp1
 
---funcion que dado dos polinomios, devuelve la suma de estos
+--funcion que dado dos polinomios, devuelve la suma de estos dos
 sumar :: Polinomio -> Polinomio -> Polinomio
 sumar [] p2 = p2;
 sumar p1 [] = p1;
 sumar p1 p2 = sumarLength p1 (length(p1)) p2 (length(p2))
 
 
---funcion que dado una lista de polinomios, devuelve la suma de estos
+--Funcion que dado una lista de polinomios, devuelve la suma de estos
 padd :: [Polinomio] -> Polinomio
---padd lp
 padd [] = []
 padd (x:[]) = x
 padd (x:xs) = sumar x (padd xs)
 
---Funcion que multiplica un numero por un polinomio
+{-
+--Eliminamos esta funcion y la incluimos en el let de la funcon multiplicar, 
+de esta forma elimnamos una función y la reducimos a una linea.
+
+Funcion que multiplica un numero por un polinomio
 multSimple :: Float -> Polinomio -> Polinomio
 multSimple num pol = map (*num) pol
+-}
 
---concatena un 0 para tener el polinomio adecuado para sumar
+--concatena un 0 para tener el polinomio adecuado para sumar a la hora de multiplicar
 multiplicarAntigua :: Polinomio -> Polinomio
 multiplicarAntigua p = p ++ [0]
 
@@ -50,26 +64,30 @@ multiplicar [] p2 = []
 multiplicar p1 [] = []
 multiplicar p1 p2 = let 
 							--multiplicamos como en el cole, desde las unidades
-							pVecesP2 = multSimple (last p1) p2
+							--pVecesP2 = multSimple (last p1) p2
+							pVecesP2 = map (*(last p1)) p2
 							--recursividad quitando la cifra multiplicada antes
 							multResto = multiplicarAntigua ( multiplicar (init p1) p2 )
 						in 
 							sumar pVecesP2 multResto
 
---Funcion que dada una lista de varios polinomios, los multiplica
+--Funcion que dada una lista de varios polinomios, los multiplica y devuelve un polinomio.
 pmul :: [Polinomio] -> Polinomio
 pmul [] = []
 pmul (x:[]) = x
 pmul (x:xs) = multiplicar x (pmul xs)
 
+--Funcion que dado un polinomio y el valor de 'x', devuelve el resultado de evaluar el polinomio.
 peval :: Polinomio -> Float -> Float
 peval [] a = 0.0
 peval p num = (last p) + peval ( init (map (*num) p) ) num
 
+--Funcion auxiliar para calcular la derivada de un polinomio dado este y su longitud.
 calcularDerivada :: Polinomio -> Int -> Polinomio
 calcularDerivada [] _ = []
 calcularDerivada (x:restoPol) tam = [x*(fromIntegral(tam))] ++ calcularDerivada restoPol (tam-1)
 
+--Función que calcula la derivada de un polinomio
 pderv :: Polinomio -> Polinomio
 pderv [] = []
 pderv p = calcularDerivada (init p) (length(p)-1)
--- practica5_XXXXXX/TupleListPolynomial.hs
+++ practica5_XXXXXX/TupleListPolynomial.hs
@@ -1,47 +1,58 @@
+{-
+	AUTORES: HAYK KOCHARYAN (757715) & JAVIER SALAMERO SANZ (756868)
+	PRACTICA 5 TECNOLOGÍA DE LA PROGRAMACIÓN
+	FECHA DE MODIFICACIÓN: 24 / 05 / 2019
+-}
 module TupleListPolynomial where
 
 type TupleList = [(Float, Integer)]
 
 --definir funciones	
 
+--Fucion que devuelve polinomio de grado 1
 x :: TupleList
 x =  [ (1.0, 1) ]
 
--------------------------------------------------------
+--------------------------------------------------------------------------------------
+--Funcion que devuelve polinomio de grado 0
 coef :: Float -> TupleList
 coef a = [(a, 0)]
 
--------------------------------------------------------
---Funcion que evalua una ecuación dado el valor de x
+--------------------------------------------------------------------------------------
+--Funcion que evalua un polinomio, dado el valor de x
 peval :: TupleList -> Float -> Float
 peval [] _ = 0
 peval (a:xs) n = ( (n^snd(a))*fst(a) ) + (peval xs n)
 
--------------------------------------------------------
---Funcion que calcula la derivada de una lista de tuplas
+--------------------------------------------------------------------------------------
+--Funcion que calcula la derivada de un polinomio
 pderv :: TupleList -> TupleList
 pderv [] = []
 pderv (a:xs) = 
 	if snd(a) == 0 then []
 	else [ (fst(a)*fromInteger(snd(a)), snd(a)-1) ] ++ pderv xs
 
--------------------------------------------------------
+--------------------------------------------------------------------------------------
+--Funcion que dado una lista de polinomios, devuelve la suma de todas estas
 padd :: [TupleList] -> TupleList
 padd [] = []
 padd (x:[]) = x
 padd (a:xs) = suma a (padd xs)
 
+--Funcion que dado dos tuplas, devuelve la suma de estas
 suma :: TupleList -> TupleList -> TupleList
-suma [] b = b
-suma a [] = a
-suma a b = 
-	if (snd(head a)) > (snd(head b)) 
-		then [ (head a) ] ++ suma (tail a) b
-	else if (snd(head a)) < (snd(head b))
-		then [ (head b) ] ++ suma a (tail b)
-	else [ ( (fst(head a) + fst(head b)), snd(head b) )] ++ suma (tail a) (tail b)
+suma [] p2 = p2
+suma p1 [] = p1
+suma p1 p2 = 
+	--si la tupla a1 es de mayor grado que la tupa a2
+	if (snd(head p1)) > (snd(head p2)) 
+		--entonces cojo el primero de a1 y sumo lo demás
+		then [ (head p1) ] ++ suma (tail p1) p2
+	else if (snd(head p1)) < (snd(head p2))
+		then [ (head p2) ] ++ suma p1 (tail p2)
+	else [ ( (fst(head p1) + fst(head p2)), snd(head p2) )] ++ suma (tail p1) (tail p2)
 
--------------------------------------------------------
+--------------------------------------------------------------------------------------
 --devuelve la lista resultante de multiplicar la base de uno de los elementos
 --de un polinomio por las bases de otro polinomio
 bases :: Float -> [Float] -> [Float]
@@ -57,11 +68,13 @@ exponentes exp expPoli = map (exp+) expPoli
 mult2 :: TupleList -> TupleList -> TupleList
 mult2 ((a,b):p2s) p2 = zip (bases a (map fst p2)) (exponentes b (map snd p2))
 
+--Dado dos polinomios, devuelve la multiplicación de estas
 mult :: TupleList -> TupleList -> TupleList
 mult [] p2 = []
 mult p1 [] = []
 mult p1 p2 = suma (mult2 ([head(p1)]) p2) (mult (tail(p1)) p2)
 
+--Devuelve el Polinomio resultante de multiplicar una lista con 1 o más polinomios
 pmul :: [TupleList] -> TupleList
 pmul [] = []
 pmul (x:[]) = x
